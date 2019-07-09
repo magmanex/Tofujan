92084439cac2b73f1f2b77b808e26266730871e6
arinchai nanjaruwong <ashira@arinchais-MacBook-Pro.local>
2019-07-10 00:19:29 +0700

    try to add bot

--- api_tofu/urls.py
+++ api_tofu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('webhooks/tutorial/', csrf_exempt(views.TutorialBotView.as_view())),
 ]
--- api_tofu/views.py
+++ api_tofu/views.py
@@ -1,13 +1,64 @@
-# import telebot
-from django.http import HttpResponse
+from django.http import HttpResponse , JsonResponse
 from django.shortcuts import render
-bot_token = '724381439:AAHoioQIvXoFF-tN9oNaO-oYQmqPO0Rp7WM'
+from django.views import View
 
+from .models import BossTime
+
+import json
+import os
+import requests
+
+bot_token = '724381439:AAHoioQIvXoFF-tN9oNaO-oYQmqPO0Rp7WM'
+TELEGRAM_URL = "https://api.telegram.org/bot"
+# TUTORIAL_BOT_TOKEN = os.getenv('724381439:AAHoioQIvXoFF-tN9oNaO-oYQmqPO0Rp7WM', "error_token")
 
 # bot = telebot.TeleBot(token = bot_token)
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == 'GET':
+        return HttpResponse("ok")
+    def post(self, request, *args, **kwargs):
+        t_data = json.loads(request.body)
+        t_message = t_data["message"]
+        t_chat = t_message["chat"]
 
+        msg = "test"
+        # self.send_message(msg, t_chat["id"])
+        return HttpResponse("ok")
+
+    @staticmethod
+    def send_message(message, chat_id):
+        data = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "Markdown",
+        }
+        response = requests.post(
+            f"{TELEGRAM_URL}{TUTORIAL_BOT_TOKEN}/sendMessage", data=data
+        )
+
+class TutorialBotView(View):
+    def post(self, request, *args, **kwargs):
+        t_data = json.loads(request.body)
+        print(type(t_data))
+        t_message = t_data.get('message')
+        t_chat = t_data.get('chat')
+
+        msg = "test"
+        self.send_message(msg, t_chat["id"])
+        return HttpResponse("ok")
+
+    @staticmethod
+    def send_message(message, chat_id):
+        data = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "Markdown",
+        }
+        print(f"{TELEGRAM_URL}{bot_token}/sendMessage")
+        response = requests.post(
+            f"{TELEGRAM_URL}{bot_token}/sendMessage", data=data
+        )
+        print(response  )
 # @bot.message_handler(commands=['start'])
 # def send_welcome(message):
 #     bot.reply_to(message, "Testt")
